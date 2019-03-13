f9383bc69ce963932db1d58af81bdf1986328af9
Reginald Peterson <tao.rep@gmail.com>
2019-03-13 17:44:14 -0400

    remove nav icons

---
+++ htdocs/home.vue
@@ -0,0 +1,47 @@
+<div id="hero" style="position:relative">
+    <img style="padding-left:2px;position:absolute;top:0px;" name="hm_tagline" src="pics/Fibro_banner.svg" width="100%" />
+    <img style="padding-left:2px;" name="Iko_hero" src="pics/iko-twin-station-2.jpg" width="100%" />
+    <span class="logo-bold" style="color:white;position:absolute;bottom:25px;left:15px;">Meet IKo&trade;</span>
+    <span class="red" style="float:left;font-weight:bold;color:white;position:absolute;bottom:10px;left:15px;">Compact.
+        Inexpensive. And it works.</span>
+    <a style="float:right;position:absolute;right:15px;bottom:10px;color:white;text-decoration:none" class="button"
+        href="meet-iko.html">Learn More <span class="fa fa-arrow-right"></span></a>
+</div>
+
+<h3 style="font-style:italic"> ECSI designs, builds and supplies efficient, precision benchtop
+    equipment for electroplating of MICRO- and NANO-structures. <span class="big_red">FIBRotools&trade;</span>
+    are an ideal solution for academic and R&amp;D institutions seeking rapid optimization of novel
+    designs for MEMS and NANO structures and advanced high-density interconnects (HDI).
+    Publish and commercialize innovations faster with our uniform, reproducible electroplating process.
+</h3>
+
+<p>
+    What enables us to do this at a low cost is our proprietary <span class="big_red">FIBRo&trade;
+        -- Fiber-Initiated &nbsp;Boundary-layer Removal --</span> technology. Complemented
+    by our advanced wafer-holder design, <span class="big_red">FIBRo&trade;</span> produces uniformity,
+    eliminates bubbles and minimizes edge effects. <span class="big_red">FIBRo&trade;</span> is based on
+    the work of our founder and CEO, Igor V. Kadija, whose discoveries in the physicochemical
+    hydrodynamics of wet processing were awarded three US
+    patents with worldwide patent coverage.</p>
+
+<p><a href="addonoptions.html">Flexible and adaptable</a> to many different process configurations,
+    <span class="big_red">FIBRo&#8482;</span> enables fast track R&amp;D in design advancements of
+    a wide variety of MEMS, NANO, and HDI devices. <a href="meet-iko.html">Click here</a>
+    to find out more about our <a href="meet-iko.html">products</a> and <a href="addonoptions.html">add-on
+        options</a>.
+</p>
+
+<!-- Footer  -->
+<div id="bottom_nav" style="margin-top:20px;">
+    <hr />
+    <a href="index.html">Home</a> &#8226;
+    <a href="meet-ecsi.html">About</a> &#8226;
+    <a href="fibro.html">Technology</a> &#8226;
+    <a href="meet-iko.html">Products</a> &#8226;
+    <a href="news.html">News</a> &#8226;
+    <a href="qa.html">FAQ</a> &#8226;
+    <a href="price.html">Cost</a> &#8226;
+    <a href="contact.html">Contact</a> &#8226;
+    <a href="customers.html">Customers</a>
+    <div style="margin-top:10px;text-align:center">&copy; 2015-2018 ECSI Fibrotools</div>
+</div>
