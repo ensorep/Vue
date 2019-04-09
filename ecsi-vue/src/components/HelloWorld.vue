3fa7f6bcbb5b2ffd13f2ff752615412ae3ed91bb
Reggie Peterson <reginald.peterson2@nih.gov>
2019-04-09 15:51:48 -0400

    IDK again

--- ecsi-vue/src/components/HelloWorld.vue
+++ ecsi-vue/src/components/HelloWorld.vue
@@ -1,33 +1,13 @@
 <template>
-  <div id="main_content">
-    <div id="homepage">
-      <div id="hero"><img id="hero-banner" name="hm_tagline" src="pics/Fibro_banner.svg" width="100%"> <img
-          id="hp_hero" name="Iko_hero" src="pics/iko-twin-station-2.jpg"> <span
-          class="logo-text logo-text-header">Meet IKo™</span> <span class="logo-text logo-text-sub">Compact.
-          Inexpensive. And it works.</span> <a id="learnmore" href="products.html" class="button-learnmore">Learn More
-          <span class="fa fa-arrow-right"></span></a></div>
-      <h3 class="italic"> ECSI Fibrotools designs, builds ,and supplies efficient, precision benchtop
-        equipment for electroplating of MICRO- and NANO-structures. <span class="big_red">FIBRotools™</span>
-        are an ideal solution for academic and R&amp;D institutions seeking rapid optimization of novel
-        designs for MEMS and NANO structures and advanced high-density interconnects (HDI).
-        Publish and commercialize innovations faster with our uniform, reproducible electroplating process.
-      </h3>
-      <p>
-        What enables us to do this at a low cost is our proprietary <span class="big_red">FIBRotools™
-          -- Fiber-Initiated &nbsp;Boundary-layer Removal --</span> technology. Complemented
-        by our advanced wafer-holder design, <span class="big_red">FIBRotools™</span> produces uniformity,
-        eliminates bubbles and minimizes edge effects. <span class="big_red">FIBRotools™</span> is based on
-        the work of our founder and CEO, Dr. Igor V. Kadija, whose discoveries in the physicochemical
-        hydrodynamics of wet processing were awarded three US
-        patents with worldwide patent coverage.</p>
-      <p><a href="addonoptions.html">Flexible and adaptable</a> to many different process configurations,
-        <span class="big_red">FIBRotools™</span> enables fast track R&amp;D in design advancements of
-        a wide variety of MEMS, NANO, and HDI devices. <a href="products.html">Click here</a>
-        to find out more about our <a href="products.html">products</a> and <a href="addonoptions.html">add-on
-          options</a>.
-      </p>
-    </div>
-  </div>
+   <div class="hero">
+        <div class="hero-img">
+          <div class="hero_focus">
+            <h3 class="hero_focus-main"> Meet IKo<span id="tm">&trade;</span></h3>
+            <span class="hero_focus-subline">Compact. Inexepensive. Simply unmatched.</span>
+            <div class="CTA_home"><button class="CTA_home-learn">Learn More</button></div>
+          </div>
+        </div>
+      </div>
 </template>
 
 <script>
@@ -41,21 +21,50 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  h3 {
-    margin: 40px 0 0;
-  }
+html, body { margin: 0; padding: 0; font-family: Arial, Helvetica, sans-serif; }
 
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
+a { text-decoration: none; color: #000; }
 
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
+.navbar { display: -webkit-inline-box; display: -ms-inline-flexbox; display: inline-flex; -webkit-box-pack: justify; -ms-flex-pack: justify; justify-content: space-between; width: 100%; }
 
-  a {
-    color: #42b983;
-  }
+.navbar-left { -webkit-box-flex: 1; -ms-flex: 1; flex: 1; }
+
+.navbar_footer { margin-top: 1rem; text-align: center; }
+
+.navbar_footer-copyright { margin-top: 1rem; }
+
+.navbar_logo { margin: .5rem; display: inline-block; }
+
+.navbar_logo-ECSI { color: #993300; font-size: 4rem; margin: 0; padding: 0; letter-spacing: 3px; }
+
+.navbar_logo-Fibrotools { margin: 0; padding: 0; font-size: 1.25rem; text-align: center; position: relative; bottom: .5rem; }
+
+.navbar .nav-ul { display: inline-block; margin: auto; padding: 0; }
+
+.navbar .nav-ul .nav-a .nav-item { list-style: none; display: inline; padding-right: 2rem; font-size: 1.1rem; }
+
+.hero { position: relative; }
+
+.hero-img { background: url("../assets/iko-twin-station-2.jpg"); background-attachment: fixed; background-size: contain; width: 100%; height: 700px; margin: auto; }
+
+.hero_focus { position: relative; padding: 2rem; top: 50%; left: 50%; -webkit-transform: translate(-50%, -50%); transform: translate(-50%, -50%); background-color: rgba(0, 0, 0, 0.7); float: left; text-align: center; }
+
+.hero_focus-main { padding: 0; display: block; color: #fff; position: relative; font-size: 5rem; margin: auto; text-align: center; }
+
+.hero_focus-main #tm { position: absolute; top: 1rem; right: 0; font-size: 2rem; }
+
+.hero_focus-subline { text-align: center; color: #fff; font-size: 1.25rem; }
+
+.CTA { width: 100%; height: 200px; display: -webkit-box; display: -ms-flexbox; display: flex; }
+
+.CTA_sub { margin: auto; font-size: 1.5rem; }
+
+.CTA_home { display: block; margin: auto; }
+
+.CTA_home-learn { position: relative; width: 30%; -webkit-transition: .5s; transition: .5s; padding: 1rem; border-radius: 4px; font-size: 1rem; margin: 1.5rem 0 0 0; background: #993300; border: none; color: #fff; text-align: center; }
+
+.CTA_home-learn:hover { -webkit-transition: .5s; transition: .5s; width: 50%; }
+
+.clients { display: -webkit-box; display: -ms-flexbox; display: flex; -ms-flex-pack: distribute; justify-content: space-around; -webkit-box-align: center; -ms-flex-align: center; align-items: center; }
+/*# sourceMappingURL=styles.min.css.map */
 </style>
