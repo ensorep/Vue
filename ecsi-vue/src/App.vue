79f10f700398d7c331bde1f011d31b3bd903bd09
Taorep <tao.rep@gmail.com>
2019-06-07 14:27:58 -0400

    begin merging to folio

--- ecsi-vue/src/App.vue
+++ ecsi-vue/src/App.vue
@@ -1,33 +1,5 @@
 <template>
-  <div id="app">
-    <div class="navbar">
-      <div class="navbar-left">
-        <div class="navbar_logo">
-          <h1 class="navbar_logo-ECSI">ECSI</h1>
-          <h4 class="navbar_logo-Fibrotools">Fibrotools, Inc. </h4>
-        </div>
-      </div>
-      <ul class="nav-ul navbar-right">
-        <a class="nav-a"><li class="nav-item"><router-link to="/">Home</router-link></li></a>
-        <a class="nav-a"><li class="nav-item"><router-link to="/About">About</router-link></li></a>
-        <a class="nav-a"><li class="nav-item"><router-link to="/About">Technology</router-link></li></a>
-        <a class="nav-a"><li class="nav-item"><router-link to="/About">Products</router-link></li></a>
-        <a class="nav-a"><li class="nav-item"><router-link to="/About">News</router-link></li></a>
-        <a class="nav-a"><li class="nav-item"><router-link to="/About">FAQ</router-link></li></a>
-        <a class="nav-a"><li class="nav-item"><router-link to="/About">Contact</router-link></li></a>
-      </ul>
-    </div>
-    <router-view/>
-    <div class="navbar_footer">
-      <a href="about.html">About</a> &#8226;
-      <a href="technology.html">Technology</a> &#8226;
-      <a href="products.html">Products</a> &#8226;
-      <a href="news.html">News</a> &#8226;
-      <a href="faq.html">FAQ</a> &#8226;
-      <a href="contact.html">Contact</a>
-      <div class="navbar_footer-copyright">&copy; ECSI Fibrotools</div>
-    </div>
-  </div>
+
 </template>
 
 <style>
